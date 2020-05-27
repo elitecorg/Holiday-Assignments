ebd09b1ae3518cb9fc36b88d4f5e7929b0321bdd
elitecorg <65866826+elitecorg@users.noreply.github.com>
2020-05-27 18:57:10 +0530

    Delete question_15.java

--- question_15.java
+++
@@ -1,7 +0,0 @@
-//the primitive types and their corresponding object wrapper classes. For example. If the conversion goes the other way, this is called unboxing.
-public class question_15
-{
-    public static void main(String args[])
-    {
-        Character ch = 'a'; // is an example of autoboxing
-        
